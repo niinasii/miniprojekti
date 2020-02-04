d02a786eafeb2d74d635a7132a6e69ef3d4fee6a
Otto Eklund <ottoeklund@gmail.com>
2020-02-04 13:42:44 +0200

    Update users.js

--- ostoslistaApp/routes/users.js
+++ ostoslistaApp/routes/users.js
@@ -4,7 +4,6 @@ var router = express.Router();
 var fetch = require('node-fetch');
 
 //hakee api musesta tiedot
-router.route('/kuvahaku/:id')
   .get(function (req, res) {
     // res.send('respond with a resource');
     fetch("https://pixabay.com/api/?key=15135527-ee5c1abe508d61f100ff75126=" + req.params.hakusana)
