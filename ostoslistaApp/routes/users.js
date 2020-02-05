f0f14433136f487017b50521ba6f140e1ac9ded6
jaakko koskela <jaska.koskela@gmail.com>
2020-02-05 11:17:30 +0200

    ei mitään committoitavaa

--- ostoslistaApp/routes/users.js
+++ ostoslistaApp/routes/users.js
@@ -2,36 +2,49 @@ var express = require('express');
 var router = express.Router();
 //pitää olla fetch täällä
 var fetch = require('node-fetch');
+let fs = require('fs')
 
+ostoslista = [];
+
+router.get('/', function(req, res, next) { // tämän pitäisi palauttaa olemassa olevan ostoslistan käyttäjälle selaimen latautuessa - jaska
+  res.json(ostokset.json);
+
+}
 //hakee api musesta tiedot
-router.route('/:hakusana')
+router.route('/:hakusana') 
   .get(function (req, res) {
     const haku = req.params.hakusana
+    ostoslista.push(haku);
+    console.log(ostoslista)
     fetch("https://pixabay.com/api/?key=15135527-ee5c1abe508d61f100ff75126&q=" + haku + "&image_type=illustration&lang=fi&category=food")
       .then(function (fetchres) {
-        console.log(fetchres)
-        //console.log(res.json())
-        //muuttaa jsoniksi?
         return fetchres.json()
       })
-      //lähettää kuvan localhostille selaimelle
+      //lähettää kuvan localhostiin selaimelle
       .then(function (data) {
-        console.log(data)
+        // console.log(data)
         res.json(data)
-        fs.writeFile('ostokset.json', JSON.stringify(req.body), () => { console.log("Ostokset tallennettu") })
+        fs.writeFile('ostokset.json', JSON.stringify(ostoslista), () => { console.log("Ostokset tallennettu") })
       })
   })
-router.post('/', function (req, res, next) {
-  console.log("post pyyntö laitettu");
-  console.dir(req.body);
-  // var ostos = req.body;
-  // ostoslista.push(henkilo);
-  // console.dir(henkilolista);
-  fs.writeFile('ostokset.json', JSON.stringify(req.body), () => { console.log("Ostokset tallennettu") })
-  // res.render('kuittaus', { title: 'kiitos, '+henkilo.name });
-  // res.send(`<h1>Post: Henkilon tiedot: ${req.body.name}, ${req.body.email}, ${req.body.gender}.<h1>`);
-  // res.render('lomake', { title: 'lomake', vastaanotettu: `Kiitos!<br>Nimi: ${req.body.name}<br>s-posti: ${req.body.email}<br>Sukupuoli: ${req.body.gender}` });
+
+  router.put('/', function(req, res, next){ // ensimmäinen versio PUT:sta, en tiedä toimiiko : D - jaska 
+    let ostosind=ostoslista.findIndex(o=>o.hakusana==req.params.hakusana);
+    ostoslista[ostosind]=req.body
+    res.json({message:ostosind+ " updated"});
 })
+
+// router.post('/', function (req, res, next) {
+//   console.log("post pyyntö laitettu");
+//   console.dir(req.body);
+//   // var ostos = req.body;
+//   // ostoslista.push(henkilo);
+//   // console.dir(henkilolista);
+//   fs.writeFile('ostokset.json', JSON.stringify(req.body), () => { console.log("Ostokset tallennettu") })
+//   // res.render('kuittaus', { title: 'kiitos, '+henkilo.name });
+//   // res.send(`<h1>Post: Henkilon tiedot: ${req.body.name}, ${req.body.email}, ${req.body.gender}.<h1>`);
+//   // res.render('lomake', { title: 'lomake', vastaanotettu: `Kiitos!<br>Nimi: ${req.body.name}<br>s-posti: ${req.body.email}<br>Sukupuoli: ${req.body.gender}` });
+// })
 
 
 module.exports = router;
