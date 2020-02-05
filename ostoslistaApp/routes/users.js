d9959af55deab62a350291108a4b64efc3ae9a78
Suvisp <suvi.s.partanen@gmail.com>
2020-02-05 13:54:50 +0200

    ei lisättävää, uuden version haku

--- ostoslistaApp/routes/users.js
+++ ostoslistaApp/routes/users.js
@@ -13,7 +13,7 @@ router.route('/:hakusana').get(function (req, res) {
   const haku = req.params.hakusana
   fetch("https://pixabay.com/api/?key=15135527-ee5c1abe508d61f100ff75126&q=" + haku + "&image_type=illustration&lang=fi&category=food")
     .then(function (fetchres) {
-      console.log(fetchres)
+      // console.log(fetchres)
       //muuttaa jsoniksi
       return fetchres.json()
     })
@@ -23,13 +23,13 @@ router.route('/:hakusana').get(function (req, res) {
       res.json(data)
     })
 }) //lisää ostokset arrayhin palvelimelle ostokset.json 
-  .post('/', function (req, res, next) {
+  .post(function (req, res, next) {
     let tuote = req.body
     ostoslista.push(tuote)
     console.log(ostoslista)
     fs.writeFile('ostokset.json', JSON.stringify(ostoslista), () => { console.log("Ostokset tallennettu") })
 
-  }).delete('/', function (req, res) {
+  }).delete(function (req, res) {
     //poistaa ostoksen
     for (var o in ostokset) {
       if (ostokset[o].hakusana == req.body.hakusana) {
@@ -42,7 +42,7 @@ router.route('/:hakusana').get(function (req, res) {
     }
     res.json("{'msg': 'Ostoslistalta ei löydy tätä tuotetta!'}");
   })
-  .put('/', function (req, res, next) { // ensimmäinen versio PUT:sta, en tiedä toimiiko : D - jaska 
+  .put(function (req, res, next) { // ensimmäinen versio PUT:sta, en tiedä toimiiko : D - jaska 
     let ostosind = ostoslista.findIndex(o => o.hakusana == req.params.hakusana);
     ostoslista[ostosind] = req.body
     res.json({ message: ostosind + " updated" });
